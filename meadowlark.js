a64996dbb0cb5ce71f7f96ab350ed20ee5d26a29
Julian Cruz <freakcruz@me.com>
2016-07-20 23:44:31 -0500

    first commit

--- meadowlark.js
+++ meadowlark.js
@@ -5,6 +5,28 @@ var fortune = require('./lib/fortune.js');
 //set up handlebars view engine
 var handlebars =  require('express-handlebars').create({ defaultLayout:'main'});
 
+app.use(require('body-parser')());
+
+app.get('/newsletter', function(req, res){
+	// we will learn about CSRF later...for now, we just
+	// provide a dummy value
+	res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function(req, res){
+	if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    }else{
+    	// if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -17,7 +39,25 @@ app.get('/', function(req, res){
 });
 
 app.get('/about', function(req, res){
-	res.render('about', { fortune: fortune.getFortune() });
+	res.render('about', { 
+			fortune: fortune.getFortune(),
+			pageTestScript: '/qa/tests-about.js'
+	 });
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
+
+app.get('/headers', function(req, res){
+	res.set('Content-Type', 'text/plain');
+	var s = '';
+	for(var name in req.headers) s += name + ':' + req.headers[name] + '\n';
+	res.send(s);
 });
 
 // custom 404 page , 404 catch-all handler (middleware)
